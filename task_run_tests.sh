57684ed181e00fc4ef1830fd8d0596a83cc3a176
nvasilev98 <nikolayvasilev1234@gmail.com>
2020-07-02 12:34:37 +0300

    configure folders

--- task_run_tests.sh
+++ task_run_tests.sh
@@ -5,7 +5,7 @@ set -e
 # task script is in resource-tutorial/job-inputs/ folder
 # application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
 # $GOPATH is gopath/ folder
-export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app/Godeps/_workspace
+export GOPATH=$(pwd)/gopath:$(pwd)/gopath/src/github.com/cloudfoundry-community/simple-go-web-app
 cd gopath/src/github.com/cloudfoundry-community/simple-go-web-app/
 
 go test ./...
